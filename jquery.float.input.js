186287406e2a2f901e2528112ec7000c24ed3405
Mohamed Usman <emyu10@gmail.com>
2015-05-01 18:37:46 +0500

    this is the commit with the actual javascript code

--- jquery.float.input.js
+++ jquery.float.input.js
@@ -1,1 +1,23 @@
-
+/**
+ * @author Mohamed Usman
+ * @description jQuery Plugin to limit input box to floating point values only
+ * @email emyu10@gmail.com
+ */
+ 
+(function($){
+	'use strict';
+	$.fn.floatinput = function(){
+		var old = $.fn.floatinput;
+		this.keypress(function(event){
+			if ((event.which != 46 || $(this).val().indexOf('.') != -1) && (event.which < 48 || event.which > 57)) {
+				event.preventDefault();
+			}
+		});
+		return this;
+		
+		$.fn.floatinput.noConflict = function () {
+			$.fn.floatinput = old
+			return this
+		}
+	}
+})(jQuery);
